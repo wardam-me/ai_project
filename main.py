3091853164e9d5c34b1acf6613fca6cdefc7ac05
MX76 <39699562-MX76@users.noreply.replit.com>
2025-02-26 15:20:15 +0000

    Configure application to run on port 5000 and add a new port 5001 for SocketIO.
    Screenshot: https://storage.googleapis.com/screenshot-production-us-central1/360d4eb4-95db-466a-9ef7-cf9ce420aad2/f5b6e3be-351c-4c35-b1e8-25e870fdba7b.jpg

--- main.py
+++ main.py
@@ -378,4 +378,4 @@ def inject_translations():
 
 if __name__ == '__main__':
     os.makedirs(CONFIG_DIR, exist_ok=True)
-    socketio.run(app, host='0.0.0.0', port=5001, debug=True)
+    socketio.run(app, host='0.0.0.0', port=5000, debug=True, allow_unsafe_werkzeug=True)
